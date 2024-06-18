fae16061614b555a049b55c37eb247069c748af8
Elizabeth Porter <142852984+1819367@users.noreply.github.com>
2024-06-18 16:40:12 -0700

    Added recipe full component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
 import RecipeExcerpt from "./components/Recipe.Excerpt";
+import RecipeFull from "./components/RecipeFull";
 import "./App.css";
 
 const App = () => {
   const [recipes, setRecipes] = useState([]);
+  const [selectedRecipe, setSelectedRecipe] = useState(null);
 
   useEffect(() => {
     const fetchAllRecipes = async () => {
@@ -25,14 +27,26 @@ const App = () => {
     fetchAllRecipes();
   }, []);
 
+  const handleSelectRecipe = (recipe) => {
+    setSelectedRecipe(recipe);
+  };
+
+  const handleUnselectRecipe = () => {
+    setSelectedRecipe(null);
+  };
+
   return (
     <div className='recipe-app'>
       <Header />
-      <div className="recipe-list">
+      {selectedRecipe && <RecipeFull selectedRecipe={selectedRecipe} handleUnselectRecipe={handleUnselectRecipe} />}
+    
+      {!selectedRecipe && (
+        <div className="recipe-list">
         {recipes.map((recipe) => (
-          <RecipeExcerpt key={recipe.id} recipe={recipe} />
-        ))}
-      </div>
+            <RecipeExcerpt key={recipe.id} recipe={recipe} handleSelectRecipe={handleSelectRecipe}/>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
